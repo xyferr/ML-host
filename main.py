1d732620fac28fed373289d2f7b79256d9992a6c
xyferr <rohit22153105@akgec.ac.in>
2023-10-29 12:26:45 +0530

    upddd

--- main.py
+++ main.py
@@ -1,5 +1,41 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from model import Spotify_Recommendation  # Importing recommendation model
+import numpy as np
+import pandas as pd
+import json
+
+class SongRequest(BaseModel):
+    s_name: str
+
+app = FastAPI()
+
+
+sdf = pd.read_csv("SpotifySongs.csv")
+recommendations = Spotify_Recommendation(sdf)
+
+@app.post('/recommend')
+def rec(song_request: SongRequest):
+    song_name = song_request.s_name
+
+    if song_name:
+        recommended_songs, album_cvr = recommendations.recommend(song_name)
+        response = {
+            "recommended_songs": recommended_songs,
+            "album_covers": album_cvr
+        }
+    else:
+        response = {"error": "Unknown song name"}
+
+    return response
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+"""
+from fastapi import FastAPI
+from pydantic import BaseModel
 import json
 from model import Spotify_Recommendation  # Importing recommendation model
 import numpy as np
@@ -40,50 +76,5 @@ def rec():
         
         
 
-
-
 """
-from flask import Flask, request, jsonify
-from model import Spotify_Recommendation  # Importing recommendation model
-import numpy as np
-import pandas as pd
 
-
-app = Flask(__name__)
-
-
-sdf = pd.read_csv(r"..\ML files\CSV_files\SpotifySongs.csv")
-recommendations = Spotify_Recommendation(sdf)
-
-
-@app.route('/recommend', methods=['POST'])
-def recommend():
-    data = request.get_json()  # input data from the JSON request
-
-    if 'song_name' in data:
-        song_name = data['song_name']
-       
-        # artist_name = data.get('artist_name', None)
-        amount = data.get('amount', 1)  # Specify the number of recommendations, default to 1
-
-        # Calling recommendation function
-        recommended_songs, album_covers = recommendations.recommend(song_name, amount)
-
-        # Creating a response JSON
-        response = {
-            'recommended_songs': recommended_songs,
-            'album_covers': album_covers
-        }
-
-        #with open('output.json', 'w') as f:
-        #   json.dump(response, f)
-
-        return jsonify(response)
-    else:
-        return jsonify({'error': 'Please provide a song_name in the request data'})
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-"""
